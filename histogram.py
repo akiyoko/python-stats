ba1f59b3d8d252990505bd309b6da0bbef9e31f0
Akihito Yokose <ayokose@gmail.com>
2014-09-04 00:39:23 +0900

    2nd

--- histogram.py
+++ histogram.py
@@ -13,48 +13,106 @@ import matplotlib.pyplot as plt
 log = logging.getLogger(__name__)
 
 
-class Histogram(object):
+class StatsColor(object):
+    pass
 
-    DATA_CELL_RANGE = 'A1:A10000'
 
-    def __init__(self, data=None, csv_name=None,
-        xls_name=None, sheet_name=None, data_start_cell='A1', data_end_cell='A10000', skip_empty=True):
-        self.data = data
-        self.csv_name = csv_name
-        self.xls_name = xls_name
-        self.sheet_name = sheet_name
+class StatsBase(object):
+    pass
 
-        if self.data is None:
-            if self.xls_name is None:
+
+class StatsData(numpy.ndarray):
+    '''
+    property
+        size
+        sum
+        mu
+        median
+        sigma
+    '''
+    def __new__(cls, input_array, legend=None):
+        if isinstance(input_array, numpy.ndarray):
+            return input_array
+        return numpy.asarray(input_array).view(cls)
+
+    @property
+    def size(self):
+        return super(StatsData, self).size
+
+    @property
+    def sum(self):
+        return super(StatsData, self).sum()
+
+    @property
+    def mean(self):
+        return super(StatsData, self).mean()
+
+    #TODO
+    #@property
+    #def median(self):
+    #    return numpy.median(self)
+
+    @property
+    def std(self):
+        return super(StatsData, self).std()
+
+
+class StatsHistogram(object):
+
+    #DATA_CELL_RANGE = 'A1:A10000'
+    data = None
+    data2 = None
+    data3 = None
+    data4 = None
+    data5 = None
+
+    def __init__(
+        self,
+        data=None,
+        csv_name=None,
+        xls_name=None, sheet_name=None, data_start_cell='A1', data_end_cell='A10000', skip_empty=True
+    ):
+
+        if data is None:
+            if xls_name is None:
                 files = fnmatch.filter(os.listdir('.'), '*.xls')
                 #files = [f for f in os.listdir('.') if fnmatch.fnmatch(f, '*.xls|*.xlsx')]
                 if len(files) > 0:
                     print('@@@ files=%s' % files)
-                    self.xls_name = files[0]
+                    xls_name = files[0]
                 else:
                     raise Exception('aaaa')
-            print('@@@ self.xls_name=%s' % self.xls_name)
-            book = xlrd.open_workbook(self.xls_name)
+            print('@@@ xls_name=%s' % xls_name)
+            book = xlrd.open_workbook(xls_name)
 
-            if self.sheet_name is None:
+            if sheet_name is None:
                 sheet = book.sheet_by_index(0)
             else:
                 sheet = book.sheet_by_name('Statistics (total score)')
             print '@@@ sheet.nrows=%s' % sheet.nrows
             print '@@@ sheet.ncols=%s' % sheet.ncols
 
-            start_row, start_col, end_row, end_col = Utils.cellrange_to_rowcol_pair(self.DATA_CELL_RANGE)
+            start_row, start_col = Utils.cell_to_rowcol2(data_start_cell)
+            end_row, end_col = Utils.cell_to_rowcol2(data_end_cell)
+            #start_row, start_col, end_row, end_col = Utils.cellrange_to_rowcol_pair(self.DATA_CELL_RANGE)
             print '@@@ start_row=%s' % start_row
             print '@@@ start_col=%s' % start_col
             print '@@@ end_row=%s' % end_row
             print '@@@ end_col=%s' % end_col
-            data = []
-            for row in range(start_row, min(end_row + 1, sheet.nrows)):
-                for col in range(start_col, min(end_col + 1, sheet.ncols)):
+            datas = []
+            for col in range(start_col, min(end_col + 1, sheet.ncols)):
+                data = []
+                for row in range(start_row, min(end_row + 1, sheet.nrows)):
                     value = sheet.cell(row, col).value
                     if value != '':
                         data.append(value)
-            print '@@@ data=%s' % data
+                datas.append(data)
+#                print '@@@ data=%s' % data
+            print '@@@ datas=%s' % datas
+
+            self.data = StatsData(datas[0]) if len(datas) > 0 else None
+            self.data2 = StatsData(datas[1]) if len(datas) > 1 else None
+            self.data3 = StatsData(datas[2]) if len(datas) > 2 else None
 
 #            start_cell = Histogram.cell_to_rowcol(self.DATA_START_CELL)
 #            print '@@@ sheet.nrows=%s' % sheet.nrows
@@ -66,20 +124,16 @@ class Histogram(object):
 #            print '@@@ col_slices=%s' % col_slices
 #            print '@@@ len(col_slices)=%s' % len(col_slices)
 
-        self.data = numpy.array(data)
+        #self.data = StatsData(data)
         print 'data=%s' % self.data
-        print 'size of data=%d' % len(self.data)
-        self.mu = numpy.mean(self.data)
-        print 'mean value=%.1f' % self.mu
-        print 'median value=%.1f' % numpy.median(self.data)
-        #self.sigma = numpy.std(self.data)
-        #print 'standard deviation=%.2f' % self.sigma
+        print 'dir(data)=%s' % dir(self.data)
+        print 'size of data=%d' % self.data.size
+        print 'sum total=%.1f' % self.data.sum
+        print 'mean value=%.1f' % self.data.mean
+        #print 'median value=%.1f' % self.data.median
+        print 'standard deviation(sigma)=%.2f' % self.data.std
 
-    @property
-    def sigma(self):
-        return numpy.std(self.data)
-
-    def draw(self):
+    def draw(self, legend=False):
         # 共通初期設定
         plt.rc('font', **{'family': 'serif'})
         # キャンバス
@@ -92,7 +146,7 @@ class Histogram(object):
         ax.set_xlim(0, 50)
         ax.set_ylim(0, 20)
         # 平均値
-        ave = ax.axvline(x=self.mu, linewidth=1, color='#ff9900', linestyle='--', zorder=0)
+        ave = ax.axvline(x=self.data.mean, linewidth=1, color='#ff9900', linestyle='--', zorder=0)
         # タイトル
         ax.set_title('Histogram of Scores', size=16)
         ax.set_xlabel('Score', size=14)
@@ -118,7 +172,12 @@ class Histogram(object):
     def cell_to_rowcol(cell):
         return Utils.cell_to_rowcol2('A1')
 
+    @staticmethod
+    def sigma(data):
+        return numpy.std(data)
+
+
 
 if __name__ == '__main__':
-    histogram = Histogram()
+    histogram = StatsHistogram()
     histogram.draw()
